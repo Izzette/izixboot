3634dd2a4d53969170ac9e61b413694f560a3bc2
Isabell Cowan <isabellcowan@gmail.com>
2017-03-14 20:20:54 -0500

    Repeats "Hello World".

--- boot.s
+++ boot.s
@@ -2,16 +2,22 @@
 	cli
 	ljmp	$0,	$boot
 boot:
+say:
 	mov	$msg,	%si
 	mov	$0x0e,	%ah
-say:
+putc:
 	lodsb
 	or	%al,	%al
 	jz	halt
 	int	$0x10
-	jmp	say
+	jmp	putc
 halt:
+	mov $0x00, %ah
+	int $0x16
+	sti
 	hlt
+	cli
+	jmp	say
 msg:
-	.asciz "Hello World"
+	.asciz "Hello World\r\n"
 // vim: set ts=8 sw=8 noet syn=asm:
---
+++ compileboot.sh
@@ -0,0 +1,1 @@
+gcc -m16 -nostdlib -Wl,--oformat,binary,-T,linker.ld boot.s -o boot
