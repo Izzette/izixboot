0cd50d4e08ba4ed80d16d584d5056fa6417e3e63
Isabell Cowan <isabellcowan@gmail.com>
2017-04-11 20:03:57 -0500

    The pmode() function now initializes the stack for the kernel.

--- src/pmode.s
+++ src/pmode.s
@@ -3,9 +3,15 @@
 
 .file		"pmode.s"
 
-// Kernel entry point
-.set	kentryseg,	0x08
-.set	kentryoffset,	0x9000
+// The start of the 32-bit protected mode stack, formally the end of our bootloader.
+	.set	pmodestack,	0x8000
+
+// Data and code segments.
+	.set	dataseg,	0x10
+	.set	codeseg,	0x08
+
+// Kernel entry point.
+	.set	kentryoffset,	0x9000
 
 // We're moving into 32-bit protected mode.
 .code32
@@ -14,17 +20,27 @@
 
 	.global pmode
 	.type	pmode,		@function
-// Protected mode code, just setup segement registers and far jump to main.
+// Our snippet of protected mode code.
+// * Setup the new stack reclaiming the MBR bootstrap code.
+// * Set the segment registers to something compatible with our flat GDT.
+// * Finally far jump to our kernels _start.
 // void pmode () {
 pmode:
-	mov	$0x10,		%ax
+// Setup our protected mode stack.
+	mov	$pmodestack,	%bp
+	mov	%bp,		%sp
+
+// Set all segment registers for our GDTs data segment.
+	mov	$dataseg,	%ax
 	mov	%ax,		%ds
 	mov	%ax,		%es
 	mov	%ax,		%fs
 	mov	%ax,		%gs
 	mov	%ax,		%ss
 
-	ljmp	$kentryseg,	$kentryoffset
+// Far jump to our kernels entry point.
+// There is no need to call, as the kernels _start should never _ret.
+	ljmp	$codeseg,	$kentryoffset
 // }
 	.size	pmode,		.-pmode
 
